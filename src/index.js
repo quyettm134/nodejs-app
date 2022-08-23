02276bde32f49e6e3f013940d87932c8ef93bcd8
quyetnguyen134 <quyet.nguyen134@hcmut.edu.vn>
2022-08-23 21:11:45 +0700

    search page

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const { engine } = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Express handlebars
+app.engine('hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/user', (req, res) => {
+  res.render('user')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
